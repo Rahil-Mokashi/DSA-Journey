411b3029bb7a01e62a9529f2d522ba97bfa4b704
Rahil Mokashi <rahilmokashi07@gmail.com>
2025-07-25 15:00:01 +0530

    Added Problem: Trailing zeroes in factorial

---
+++ trailing_zero/main.cpp
@@ -0,0 +1,33 @@
+// Finding the trailing no of zeros in the factorial of input n
+
+#include <iostream>
+
+using namespace std;
+
+int find_trail(int n){
+
+    if(n == 0){
+        return 1;
+    }
+    int fact = 1;
+    int res = 0;
+    for(int i=2; i<=n; i++){
+        fact = fact * i;
+    }
+
+    while(fact%10==0){
+        res++;
+        fact = fact/10;
+    }
+    return res;
+}
+
+int main(){
+    int num;
+
+    cout<<"Enter a num: "<<endl;
+    cin>>num;
+
+    cout<<find_trail(num)<<endl;
+    return 0;
+}
