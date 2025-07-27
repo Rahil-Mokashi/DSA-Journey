364deade176839bca227923590b62f84024f555a
Rahil Mokashi <rahilmokashi07@gmail.com>
2025-07-27 16:58:20 +0530

    Added Problem: Check for Prime No

--- check_prime/main.cpp
+++ check_prime/main.cpp
@@ -13,7 +13,6 @@ bool check_prime(int n){
     if(n%2==0 || n%3==0){
         return false;
     }
-
     for(int i=5; i<sqrt(n); i = i+6){
         if(n%i==0 || n%(i+2)==0){
             return false;
