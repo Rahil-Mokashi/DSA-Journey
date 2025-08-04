3a2c0308be4c5677f8a77de8941771cf8523f203
Rahil Mokashi <rahilmokashi07@gmail.com>
2025-08-04 07:20:25 +0530

    Added Problem: Sum of digits

---
+++ recursion/sum_of_digits/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int sum_of_digits(int n)
+{  
+    if(n==0){
+        return 0;
+    }
+    return sum_of_digits(n/10) + n%10;
+}
+int main(){
+    int n;
+
+    cout<<"Enter the no: "<<endl;
+    cin>>n;
+
+    cout<<sum_of_digits(n)<<endl;
+    return 0;
+}
